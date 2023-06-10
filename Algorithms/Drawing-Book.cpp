fc0037f4a8714d51787b0d3eee846530044e69e4
Atqa Munzir <atqamz@gmail.com>
2023-06-10 07:26:02 +0700

    Drawing Book

---
+++ Algorithms/Drawing-Book.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p)
+{
+    int turn = 0;
+
+    if (p > n / 2)
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            if (i == p)
+                break;
+            if (((i - 1) % 2) == 1)
+                turn++;
+            cout << i;
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == p)
+                break;
+            if (((i + 1) % 2) == 0)
+                turn++;
+        }
+    }
+
+    return turn;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
